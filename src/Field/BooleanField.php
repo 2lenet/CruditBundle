c99388aa7fde916873088e16b6f983fec09c0490
Emilie D <emilie@2le.net>
2021-04-13 11:21:49 +0200

    traduction valeur booleen (#19)
    traduction valeur booleen

--- src/Field/BooleanField.php
+++ src/Field/BooleanField.php
@@ -6,10 +6,18 @@ namespace Lle\CruditBundle\Field;
 
 use Lle\CruditBundle\Contracts\FieldInterface;
 use Lle\CruditBundle\Dto\FieldView;
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
 
 class BooleanField implements FieldInterface
 {
+    /** @var Translator  */
+    private $translator;
 
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+    
     public function support(string $type): bool
     {
         return (in_array($type, ['boolean', 'bool', self::class]));
@@ -18,6 +26,12 @@ class BooleanField implements FieldInterface
     /** @param mixed $value */
     public function buildView(FieldView $fieldView, $value): FieldView
     {
-        return $fieldView->setStringValue(($value) ? 'crudit.yes' : 'crudit.no');
+        return $fieldView->setStringValue(($value) ? $this->trans('crudit.yes') : $this->trans('crudit.no'));
+    }
+
+
+    protected function trans(string $asset): string
+    {
+        return $this->translator->trans($asset);
     }
 }
