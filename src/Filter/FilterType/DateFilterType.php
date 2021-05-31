9c6c0b96c5b2e96648bcdee96a506c3c34fc9879
Nathan <nathan@2le.net>
2021-05-31 15:51:58 +0200

    fix: DateFilterType now working

--- src/Filter/FilterType/DateFilterType.php
+++ src/Filter/FilterType/DateFilterType.php
@@ -10,69 +10,41 @@ use Doctrine\ORM\QueryBuilder;
  */
 class DateFilterType extends AbstractFilterType
 {
-
-    protected $yearRange;
-
-
-    public function configure(array $config = [])
+    public static function new(string $fieldname): DateFilterType
     {
-        parent::configure($config);
-        $this->yearRange = (isset($config['yearRange'])) ? $config['yearRange'] : null;
-        $this->defaults['op'] = 'equal';
+        return new self($fieldname);
     }
 
-    public function setDefaults($default)
+    public function getOperators(): array
     {
-        parent::setDefaults($default);
-        switch ($this->defaults['value']) {
-            case 'now':
-                $return = date('d/m/Y');
-                break;
-            default:
-                $return = $default;
-        }
-        $this->defaults['value'] = $return;
+        return [
+            "equal" => ["icon" => "fas fa-equals"],
+            "before" => ["icon" => "fas fa-less-than"],
+            "after" => ["icon" => "fas fa-greater-than"],
+        ];
     }
-
-
 
     /**
-     * @param array  $data     The data
-     * @param string $this->id The unique identifier
+     * @param array $data The data
+     * @param string $this- >id The unique identifier
      */
     public function apply(QueryBuilder $queryBuilder): void
     {
         if (isset($this->data['value']) && $this->data['value'] && isset($this->data['op'])) {
-            $date = DateTime::createFromFormat('d/m/Y', $this->data['value']);
-            if (!$date) {
-                return;
-            }
-
             switch ($this->data['op']) {
                 case 'equal':
-                    $queryBuilder->andWhere($queryBuilder->expr()->like($this->alias . $this->columnName, ':var_' . $this->id));
+                    $queryBuilder->andWhere($queryBuilder->expr()->eq($this->alias . $this->columnName, ':var_' . $this->id));
                     break;
                 case 'before':
-                    $queryBuilder->andWhere($queryBuilder->expr()->lte($this->alias . $this->columnName, ':var_' . $this->id));
+                    $queryBuilder->andWhere($queryBuilder->expr()->lt($this->alias . $this->columnName, ':var_' . $this->id));
                     break;
                 case 'after':
                     $queryBuilder->andWhere($queryBuilder->expr()->gt($this->alias . $this->columnName, ':var_' . $this->id));
                     break;
             }
-                $queryBuilder->setParameter('var_' . $this->id, $date->format('Y-m-d') . '%');
-        }
-    }
 
-    public function getDatePickerOptions()
-    {
-        $options = [];
-        $options['changeMonth'] =  true;
-        $options['changeYear'] = true;
-        $options['dateFormat'] = 'dd/mm/yy';
-        if ($this->yearRange) {
-            $options['yearRange'] = $this->yearRange;
+            $queryBuilder->setParameter('var_' . $this->id, $this->data["value"] . '%');
         }
-        return $options;
     }
 
     public function getStateTemplate(): string
