a8d51d3077b865f58413c4c9f511f966aad038fe
Nathan <nathan@2le.net>
2021-05-31 16:15:58 +0200

    fix: DateTimeFilterType now working

--- src/Filter/FilterType/DateTimeFilterType.php
+++ src/Filter/FilterType/DateTimeFilterType.php
@@ -10,6 +10,22 @@ use Doctrine\ORM\QueryBuilder;
  */
 class DateTimeFilterType extends AbstractFilterType
 {
+    public static function new(string $fieldname): self
+    {
+        $f = new self($fieldname);
+        $f->setAdditionnalKeys(["time"]);
+
+        return $f;
+    }
+
+    public function getOperators(): array
+    {
+        return [
+            "equal" => ["icon" => "fas fa-equals"],
+            "before" => ["icon" => "fas fa-less-than"],
+            "after" => ["icon" => "fas fa-greater-than"],
+        ];
+    }
 
     /**
      * @param array  $data     The data
@@ -17,27 +33,28 @@ class DateTimeFilterType extends AbstractFilterType
      */
     public function apply(QueryBuilder $queryBuilder): void
     {
-        if (isset($data['value']) && isset($data['op'])) {
-            /** @var DateTime $datetime */
-            [$date, $time] = [null,null];
-            if (strstr($data['value']['date'], ' ')) {
-                [$date, $time] = explode(' ', $data['value']['date']);
+        if (isset($this->data['value']) && isset($this->data['op'])) {
+
+            $datetime = $this->data["value"] . " ";
+            if (isset($this->data["time"])) {
+                $datetime .= $this->data["time"];
+            } else {
+                $datetime .= "00:00:00";
             }
-            $date = empty($date) ? date('d/m/Y') : $date;
-            $time = empty($time) ? date('H:i')   : $time;
-            $datetime = DateTime::createFromFormat('d/m/Y H:i', $date . ' ' . $time);
-            switch ($data['op']) {
+
+            switch ($this->data['op']) {
+                case 'equal':
+                    $queryBuilder->andWhere($queryBuilder->expr()->eq($this->alias . $this->columnName, ':var_' . $this->id));
+                    break;
                 case 'before':
-                    $this->queryBuilder->andWhere($queryBuilder->expr()->lte($alias . $col, ':var_' . $this->id));
+                    $queryBuilder->andWhere($queryBuilder->expr()->lt($this->alias . $this->columnName, ':var_' . $this->id));
                     break;
                 case 'after':
-                    $this->queryBuilder->andWhere($queryBuilder->expr()->gt($alias . $col, ':var_' . $this->id));
-                    break;
-                case 'equal':
-                    $this->queryBuilder->andWhere($alias . $this->columnName . ' = :var_' . $this->id);
+                    $queryBuilder->andWhere($queryBuilder->expr()->gt($this->alias . $this->columnName, ':var_' . $this->id));
                     break;
             }
-            $this->queryBuilder->setParameter('var_' . $this->id, $datetime);
+
+            $queryBuilder->setParameter('var_' . $this->id, $datetime);
         }
     }
 
