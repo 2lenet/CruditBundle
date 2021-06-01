16cfebb21e12dc8895bc8c797665bbe785a51db0
Nathan <nathan@2le.net>
2021-06-01 12:12:29 +0200

    feat: PeriodeFilterType

--- src/Filter/FilterType/PeriodeFilterType.php
+++ src/Filter/FilterType/PeriodeFilterType.php
@@ -10,36 +10,25 @@ use Doctrine\ORM\QueryBuilder;
  */
 class PeriodeFilterType extends AbstractFilterType
 {
-    /**
-     * @param array  $data     The data
-     * @param string $id The unique identifier
-     */
+    public static function new(string $fieldname): self
+    {
+        $f = new self($fieldname);
+        $f->setAdditionnalKeys(["to"]);
+
+        return $f;
+    }
+
     public function apply(QueryBuilder $queryBuilder): void
     {
-        /*
         if (isset($this->data['value'])) {
-            $qb = $queryBuilder;
-            $from = $to = null;
-            $c = $this->alias . $this->columnName;
-            if (isset($this->data['value']['from']) && $this->data['value']['from']) {
-                $from = DateTime::createFromFormat($this->format, $this->data['value']['from']);
-                if ($from) {
-                    $from = $from->format('Y-m-d');
-                    $qb->andWhere($c . ' >= :var_from_' . $this->id);
-                    $queryBuilder->setParameter('var_from_' . $this->id, $from);
-                }
-            }
-            if (isset($this->data['value']['to']) && $this->data['value']['to']) {
-                $to = DateTime::createFromFormat($this->format, $this->data['value']['to']);
-                if ($to) {
-                    $to->modify('+1 day');
-                    $to = $to->format('Y-m-d');
-                    $qb->andWhere($c . ' < :var_to_' . $this->id);
-                    $queryBuilder->setParameter('var_to_' . $this->id, $to);
-                }
-            }
+            $queryBuilder->andWhere($this->alias . $this->columnName . ' >= :min_' . $this->id);
+            $queryBuilder->setParameter('min_' . $this->id, $this->data['value']);
         }
-        */
+
+        if (isset($this->data['to'])) {
+            $queryBuilder->andWhere($this->alias . $this->columnName . ' <= :max_' . $this->id);
+            $queryBuilder->setParameter('max_' . $this->id, $this->data['to']);
+        }
     }
 
     public function getStateTemplate(): string
