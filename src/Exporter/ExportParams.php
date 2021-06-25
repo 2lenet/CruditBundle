77a883b7581b4be3b606ea356143a2ea9bc17fc9
Nathan <nathan@2le.net>
2021-06-25 14:59:24 +0200

    feat: you can put headers in the exported files

--- src/Exporter/ExportParams.php
+++ src/Exporter/ExportParams.php
@@ -6,9 +6,11 @@ namespace Lle\CruditBundle\Exporter;
 
 class ExportParams
 {
-    protected $filename;
+    protected ?string $filename = null;
 
-    protected string $separator;
+    protected string $separator = ";";
+
+    protected bool $includeHeaders = true;
 
     public static function new(): self
     {
@@ -18,12 +20,12 @@ class ExportParams
     /**
      * Supported by: CSV, Excel
      */
-    public function getFilename()
+    public function getFilename(): ?string
     {
         return $this->filename;
     }
 
-    public function setFilename($filename): self
+    public function setFilename(?string $filename): self
     {
         $this->filename = $filename;
 
@@ -41,6 +43,21 @@ class ExportParams
     public function setSeparator(string $separator): self
     {
         $this->separator = $separator;
+
+        return $this;
+    }
+
+    /**
+     * Supported by: CSV, Excel
+     */
+    public function getIncludeHeaders(): bool
+    {
+        return $this->includeHeaders;
+    }
+
+    public function setIncludeHeaders(bool $includeHeaders): self
+    {
+        $this->includeHeaders = $includeHeaders;
 
         return $this;
     }
