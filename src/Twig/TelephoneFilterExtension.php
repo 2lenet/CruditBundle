54f8cc6625d0365fc8b538145b662bc45152982f
manon <manon@2le.net>
2021-07-08 14:42:52 +0200

    Telephone Extension

---
+++ src/Twig/TelephoneFilterExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lle\CruditBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class TelephoneFilterExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('telephone', [$this, 'formatTelephone'])
+        ];
+    }
+
+    public function formatTelephone($telephone)
+    {
+        $substr = substr($telephone, 1);
+        return str_replace(' ', '', '+33'.$substr);
+    }
+}
