f1eff30b4ca80bd5c4263f78d31df3c66a42cff2
Manon B <42344795+punckle@users.noreply.github.com>
2021-07-18 19:46:05 +0200

    Telephones Extensions (#72)
    * Telephone et Email Fields 2le/crudit#59
    
    * Telephone Extension
    
    * Update Telephone extensions 2le/crudit#59
    
    * Ajout champ telephoneMobile
    
    Co-authored-by: manon <manon@2le.net>

---
+++ src/Twig/CruditTelephoneFilterExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lle\CruditBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+/**
+ * This filter allows to format the display of a phone number in 03 88 .. .. ..
+ */
+class CruditTelephoneFilterExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('telephone', [$this, 'formatTelephone'])
+        ];
+    }
+
+    public function formatTelephone($telephone)
+    {
+        if (strlen($telephone) == 10) {
+            return wordwrap($telephone, 2, ' ', true);
+        }
+
+        return $telephone;
+    }
+}
