8a058de231e93c12dbda2df099acafe8ebbc739c
Lollipop <31731763+SpaghettiBolognaise@users.noreply.github.com>
2021-10-06 16:30:42 +0200

    fix: form validation client side, for required fields (#114)

---
+++ assets/sb-admin/js/form.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * Client side form validation with Bootstrap
+ */
+window.addEventListener('load', function () {
+
+    let forms = document.querySelectorAll('.needs-validation');
+
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                form.classList.add('was-validated');
+            }, false);
+        });
+});
