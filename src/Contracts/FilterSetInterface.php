01b62e261726d1ad6ce9eab70d0dbb45f01939c5
Lollipop <31731763+SpaghettiBolognaise@users.noreply.github.com>
2021-12-02 10:53:42 +0100

    feat: configurable secondary filters (#163)
    * feat: configurable secondary filters
    
    * doc: filter amount displayed
    
    * change: renamed getAmountDisplayed to getNumberDisplayed

--- src/Contracts/FilterSetInterface.php
+++ src/Contracts/FilterSetInterface.php
@@ -1,11 +1,21 @@
 <?php
 
-
 namespace Lle\CruditBundle\Contracts;
-
 
 interface FilterSetInterface
 {
+    /**
+     * An iterable of FilterTypes
+     */
     public function getFilters(): array;
+
+    /**
+     * Get the total number of filters to display, the others will be collapsed
+     */
+    public function getNumberDisplayed(): int;
+
+    /**
+     * Internal ID
+     */
     public function getId(): string;
 }
