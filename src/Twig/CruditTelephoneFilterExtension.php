361ecd17088a85a8f716e0cc511463bce3a2643d
Antoine RICHARD <antoine.richard68@gmail.com>
2022-01-14 15:14:21 +0100

    generic phone number filter

--- src/Twig/CruditTelephoneFilterExtension.php
+++ src/Twig/CruditTelephoneFilterExtension.php
@@ -17,24 +17,41 @@ class CruditTelephoneFilterExtension extends AbstractExtension
         ];
     }
 
+    /**
+     * Format phone number :
+     *  0102030405 => 01 02 03 04 05
+     *  +X102030405 => +X 1 02 03 04 05
+     *  +XX102030405 => +XX 1 02 03 04 05
+     *  +XXX102030405 => +XXX 1 02 03 04 05
+     *
+     * @param $telephone
+     * @return string
+     */
     public function formatTelephone($telephone)
     {
         $telephone = str_replace(' ', '', $telephone);
 
-        if (strpos($telephone, '+33') === 0) {
-            $start = substr($telephone, 0, 3);
-            $middle = substr($telephone, -9, 1);
-            $end = substr($telephone, 4);
-
-            $formatEnd = wordwrap($end, 2, ' ', true);
-
-            return $start . ' ' . $middle . ' ' . $formatEnd;
-        }
-        
-        if (strlen($telephone) == 10) {
-            return wordwrap($telephone, 2, ' ', true);
+        if (strpos($telephone, '+') === 0) {
+            $telephone = substr($telephone, 1);
+            $indicatorMask = str_repeat("#", strlen($telephone) - 9);
+            $telephone = "+" . $this->applyMask($indicatorMask . " # ## ## ## ##", $telephone);
+        } else {
+            $telephone = $this->applyMask("## ## ## ## ##", $telephone);
         }
 
         return $telephone;
+    }
+
+    private function applyMask(string $mask, string $value)
+    {
+        $result = ""; $i = 0; $counter = 0;
+        $lenValue = strlen($value);
+        $lenMask = strlen($mask);
+        while ($counter < $lenValue && $i < $lenMask) {
+            $char = $mask[$i];
+            $result .= ($char === '#') ? $value[$counter++] : $char;
+            $i++;
+        }
+        return $result;
     }
 }
