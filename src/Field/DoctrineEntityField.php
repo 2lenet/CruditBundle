946893b1c3be306c3874340678017ad903e72e32
Valentin HELIES <valentin@2le.net>
2022-01-26 16:45:14 +0100

    Update DoctrineEntityField to have a link 2le/crudit#133

--- src/Field/DoctrineEntityField.php
+++ src/Field/DoctrineEntityField.php
@@ -4,16 +4,28 @@ declare(strict_types=1);
 
 namespace Lle\CruditBundle\Field;
 
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 class DoctrineEntityField extends AbstractField
 {
-
     public function support(string $type): bool
     {
+        // this field is set by the FieldResolver for Doctrine relations
         return (in_array($type, [self::class]));
     }
 
     public function getDefaultTemplate(): ?string
     {
         return '@LleCrudit/field/doctrine_entity.html.twig';
+    }
+
+    public function configureOptions(OptionsResolver $optionsResolver): void
+    {
+        parent::configureOptions($optionsResolver);
+
+        $optionsResolver->setDefaults([
+            'link' => true,
+            'route' => null,
+        ]);
     }
 }
