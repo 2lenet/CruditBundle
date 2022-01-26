c9a9c7b060e415b5a3c84b215e54ce37339c98d3
Antoine68 <38207637+Antoine68@users.noreply.github.com>
2022-01-26 09:58:00 +0100

    Markdown formtype and field (#197)
    * markdown formtype and field
    
    * fix import
    
    * sanitizer : allow null html

---
+++ assets/sb-admin/js/markdown_textarea.js
@@ -0,0 +1,10 @@
+'use strict';
+import * as EasyMDE from 'easymde';
+
+window.addEventListener('load', function () {
+
+    document.querySelectorAll("textarea.markdown-editor").forEach(textarea => {
+        new EasyMDE({element: textarea,  toolbar: ["bold", "italic", "|", "heading-smaller", "heading-bigger", "|", "unordered-list", "ordered-list", "|", "link", "quote", "code", "|", "preview"]});
+    });
+
+});
---
+++ src/Service/SanitizerService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Lle\CruditBundle\Service;
+
+use Lle\CruditBundle\Contracts\SanitizerInterface;
+use HTMLPurifier;
+use HTMLPurifier_Config;
+
+class SanitizerService implements SanitizerInterface
+{
+    public function sanitize(?string $dirtyHtml = ""): string
+    {
+        $config = HTMLPurifier_Config::createDefault();
+        $sanitizer = new HTMLPurifier($config);
+        return $sanitizer->purify($dirtyHtml);
+    }
+
+}
