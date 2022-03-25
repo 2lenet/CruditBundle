6d43bb956740aaab5d2735f8255d3234c0ee0078
Valentin HELIES <valentin@2le.net>
2022-03-25 15:07:29 +0100

    Add currency field 2le/crudit#179

--- src/Field/CurrencyField.php
+++ src/Field/CurrencyField.php
@@ -4,21 +4,30 @@ declare(strict_types=1);
 
 namespace Lle\CruditBundle\Field;
 
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Twig\Environment;
 
-class NumberField extends AbstractField
+class CurrencyField extends AbstractField
 {
-    
+    private RequestStack $requestStack;
+
+    public function __construct(Environment $twig, RequestStack $requestStack)
+    {
+        parent::__construct($twig);
+        $this->requestStack = $requestStack;
+    }
+
     public function support(string $type): bool
     {
-        return (in_array($type, ['float', self::class]));
+        return (in_array($type, ['decimal', 'currency', self::class]));
     }
 
     public function getDefaultTemplate(): ?string
     {
-        return '@LleCrudit/field/number.html.twig';
+        return '@LleCrudit/field/currency.html.twig';
     }
-    
+
     public function configureOptions(OptionsResolver $optionsResolver): void
     {
         parent::configureOptions($optionsResolver);
@@ -27,7 +36,10 @@ class NumberField extends AbstractField
             'decimals' => '2',
             'decimal_separator' => ',',
             'thousands_separator' => ' ',
+            'locale' => $this->requestStack->getMainRequest()->getLocale(),
+            'currency' => 'EUR',
+            'property' => null,
         ]);
     }
-    
+
 }
