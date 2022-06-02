63cadd42036bb6c08e7534672a5e3cd212eae318
Bastien MUNCK <77158590+iBast@users.noreply.github.com>
2022-06-02 14:46:27 +0200

    add support for multiple workflows with same entity (#262)
    * add support for multiple workflows with same entity
    
    * add default null
    
    Co-authored-by: Bastien Munck <bastien@2le.net>

--- src/Field/WorkflowField.php
+++ src/Field/WorkflowField.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace Lle\CruditBundle\Field;
 
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Twig\Environment;
+
 class WorkflowField extends AbstractField
 {
+    public function __construct(Environment $twig, private ?string $name = null)
+    {
+        parent::__construct($twig);
+    }
+
     public function support(string $type): bool
     {
         return (in_array($type, ["workflow", self::class]));
@@ -14,5 +22,13 @@ class WorkflowField extends AbstractField
     public function getDefaultTemplate(): ?string
     {
         return "@LleCrudit/field/workflow.html.twig";
+    }
+
+    public function configureOptions(OptionsResolver $optionsResolver): void
+    {
+        parent::configureOptions($optionsResolver);
+        $optionsResolver->setDefaults([
+            "name" => $this->name,
+        ]);
     }
 }
