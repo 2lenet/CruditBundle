c0776ce8d21a430f8def6b961ee4fb9433b56d0e
Valentin HELIES <valentin@2le.net>
2022-09-22 09:17:52 +0200

    Fixe error

--- src/Controller/TraitCrudController.php
+++ src/Controller/TraitCrudController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Lle\CruditBundle\Controller;
 
-use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\ORM\EntityManagerInterface;
 use Lle\CruditBundle\Contracts\CrudConfigInterface;
 use Lle\CruditBundle\Datasource\DatasourceParams;
