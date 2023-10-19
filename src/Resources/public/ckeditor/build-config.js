ca1e0ac3c4830f9858eb028419ceb13352bff171
Valentin HELIES <valentin@2le.net>
2023-10-19 14:57:40 +0200

    Modify ckeditor plugins + modify tests

--- src/Resources/public/ckeditor/build-config.js
+++ src/Resources/public/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) https://ckeditor.com/cke4/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) https://ckeditor.com/cke4/builder/5745398dc6516f3c3b0a51eedb3cd02e
+ * (2) https://ckeditor.com/cke4/builder/a795c94ccbc1916bad88bd4abb4736c8
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) https://ckeditor.com/cke4/builder/download/5745398dc6516f3c3b0a51eedb3cd02e
+ * (3) https://ckeditor.com/cke4/builder/download/a795c94ccbc1916bad88bd4abb4736c8
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -67,6 +67,7 @@ var CKBUILDER_CONFIG = {
 		'copyformatting' : 1,
 		'dialogadvtab' : 1,
 		'div' : 1,
+		'easyimage' : 1,
 		'editorplaceholder' : 1,
 		'elementspath' : 1,
 		'enterkey' : 1,
@@ -77,6 +78,7 @@ var CKBUILDER_CONFIG = {
 		'floatingspace' : 1,
 		'font' : 1,
 		'format' : 1,
+		'format_buttons' : 1,
 		'forms' : 1,
 		'horizontalrule' : 1,
 		'html5audio' : 1,
@@ -85,11 +87,13 @@ var CKBUILDER_CONFIG = {
 		'htmlwriter' : 1,
 		'iframe' : 1,
 		'image' : 1,
+		'imgbrowse' : 1,
 		'indentblock' : 1,
 		'indentlist' : 1,
 		'inserthtmlfile' : 1,
 		'justify' : 1,
 		'language' : 1,
+		'lineheight' : 1,
 		'link' : 1,
 		'list' : 1,
 		'liststyle' : 1,
@@ -101,6 +105,7 @@ var CKBUILDER_CONFIG = {
 		'pastefromlibreoffice' : 1,
 		'pastefromword' : 1,
 		'pastetext' : 1,
+		'pbckcode' : 1,
 		'preview' : 1,
 		'print' : 1,
 		'removeformat' : 1,
@@ -110,16 +115,19 @@ var CKBUILDER_CONFIG = {
 		'selectall' : 1,
 		'showblocks' : 1,
 		'showborders' : 1,
+		'simage' : 1,
 		'smiley' : 1,
 		'sourcearea' : 1,
 		'specialchar' : 1,
 		'stylescombo' : 1,
 		'tab' : 1,
 		'table' : 1,
+		'tableresize' : 1,
 		'tableselection' : 1,
 		'tabletools' : 1,
 		'templates' : 1,
 		'toolbar' : 1,
+		'uicolor' : 1,
 		'undo' : 1,
 		'uploadimage' : 1,
 		'wysiwygarea' : 1
