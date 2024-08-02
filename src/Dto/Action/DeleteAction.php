e14a5481d070e0dda62a69b2bb531269efb60344
Valentin HELIES <valentin@2le.net>
2024-08-02 12:09:39 +0200

    PHPSTAN

--- src/Dto/Action/DeleteAction.php
+++ src/Dto/Action/DeleteAction.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Lle\CruditBundle\Dto\Action;
 
-use Lle\CruditBundle\Dto\Path;
-
 class DeleteAction extends ItemAction
 {
     public const CALLBACK = "canDelete";
