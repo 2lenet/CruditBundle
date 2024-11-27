15c870d454abf2198c0573e569c783396dae6b18
Nathan <nathan@2le.net>
2024-11-27 16:36:01 +0100

    feat: TomSelect applied to ChoiceTypes forms

--- assets/sb-admin/js/filters.js
+++ assets/sb-admin/js/filters.js
@@ -136,5 +136,4 @@ window.addEventListener('DOMContentLoaded', function () {
             },
         );
     });
-
 });
--- assets/sb-admin/js/form.js
+++ assets/sb-admin/js/form.js
@@ -1,10 +1,12 @@
 'use strict';
 
-/**
- * Client side form validation with Bootstrap
- */
+import TomSelect from "tom-select";
+
 window.addEventListener('load', function () {
 
+    /**
+     * Client side form validation with Bootstrap
+     */
     let forms = document.querySelectorAll('.needs-validation');
 
     Array.prototype.slice.call(forms)
@@ -18,4 +20,21 @@ window.addEventListener('load', function () {
                 form.classList.add('was-validated');
             }, false);
         });
+
+    /**
+     * Apply TomSelect on choice types forms
+     */
+    document.querySelectorAll('.form-select').forEach(select => {
+        new TomSelect('#' + select.id,
+            {
+                plugins: [
+                    'remove_button',
+                ],
+                onItemAdd() {
+                    select.parentElement.querySelector('.ts-input > input').value = '';
+                    select.parentElement.querySelector('.ts-dropdown').style.display = 'none';
+                },
+            },
+        );
+    });
 });
