eb1fc1c743f82d0801d5f1e07d7e7a2dd098f30b
Valentin HÉLIES <valentin@2le.net>
2025-07-02 14:48:54 +0200

    Fix Datasource Maker 2le/crudit#534

--- src/Resources/skeleton/crud/datasource/DoctrineDatasource.php
+++ src/Resources/skeleton/crud/datasource/DoctrineDatasource.php
@@ -6,23 +6,23 @@ declare(strict_types=1);
 
 namespace <?= $namespace; ?>;
 
-<?php if ($hasFilterset) { ?>use App\Crudit\Datasource\Filterset\<?= $configSubdirectorie ?><?= $prefixFilename ?>FilterSet;<?php echo "\n"; } ?>
-use App\Entity\<?= $entityClass ?>;
+<?php if ($hasFilterset) { ?>use App\Crudit\Datasource\Filterset\<?= $configSubdirectorie ?><?= $shortEntityClass ?>FilterSet;<?php echo "\n"; } ?>
+use <?= $entityClass ?>;
 use Lle\CruditBundle\Datasource\AbstractDoctrineDatasource;
 <?php if ($hasFilterset) { ?>
 use Symfony\Contracts\Service\Attribute\Required;
 <?php } ?>
 
-class <?= $prefixFilename ?>Datasource extends AbstractDoctrineDatasource
+class <?= $shortEntityClass ?>Datasource extends AbstractDoctrineDatasource
 {
     public function getClassName(): string
     {
-        return <?= $entityClass ?>::class;
+        return <?= $shortEntityClass ?>::class;
     }
 <?php if ($hasFilterset) { ?>
 
     #[Required]
-    public function setFilterset(<?= $prefixFilename ?>FilterSet $filterSet): void
+    public function setFilterset(<?= $shortEntityClass ?>FilterSet $filterSet): void
     {
         $this->filterset = $filterSet;
     }
