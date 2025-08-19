55b0cf9cfb7e1d3750db5ca9ad15ad83b2642036
Valentin HÉLIES <valentin@2le.net>
2025-08-19 09:05:19 +0200

    Fix PHPCS

--- src/Command/ConvertEasyAdmin.php
+++ src/Command/ConvertEasyAdmin.php
@@ -6,7 +6,6 @@ use Lle\CruditBundle\Service\EasyAdminConverter\Converter;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
