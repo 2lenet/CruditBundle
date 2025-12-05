e113fa966817f811cde8513b9960c4420a5ad524
Sébastien HEITZMANN <2le@2le.net>
2025-12-05 10:43:04 +0100

    Update HistoryConfig.php

--- src/Brick/HistoryBrick/HistoryConfig.php
+++ src/Brick/HistoryBrick/HistoryConfig.php
@@ -8,6 +8,8 @@ use Lle\CruditBundle\Brick\AbstractBrickConfig;
 
 class HistoryConfig extends AbstractBrickConfig
 {
+    private string $logEntryClassName;
+
     public static function new(array $options = []): self
     {
         return new self($options);
@@ -16,5 +18,17 @@ class HistoryConfig extends AbstractBrickConfig
     public function __construct(array $options = [])
     {
         $this->options = $options;
+    }
+
+    public function getLogEntryClassName(): ?string
+    {
+        return $this->logEntryClassName;
+    }
+
+    public function setLogEntryClassName(string $className): self
+    {
+        $this->logEntryClassName = $className;
+
+        return $this;
     }
 }
